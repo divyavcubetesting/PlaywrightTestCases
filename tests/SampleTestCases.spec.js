2b850798b7618df158627f8e1f1103c37d8a5fd8
divyavcubetesting <divyavcubetesting@gmail.com>
2026-03-08 21:47:10 +0530

    Run my code

--- tests/SampleTestCases.spec.js
+++ tests/SampleTestCases.spec.js
@@ -4,23 +4,23 @@ test("test case1",async({page})=>
 {
     console.log("This is my test case1");
 })
-test("test case1",async({page})=>
+test("test case2",async({page})=>
 {
-    console.log("This is my test case1");
+    console.log("This is my test case2");
 })
-test("test case1",async({page})=>
+test("test case3",async({page})=>
 {
-    console.log("This is my test case1");
+    console.log("This is my test case3");
 })
-test("test case1",async({page})=>
+test("test case4",async({page})=>
 {
-    console.log("This is my test case1");
+    console.log("This is my test case4");
 })
-test("test case1",async({page})=>
+test("test case5",async({page})=>
 {
-    console.log("This is my test case1");
+    console.log("This is my test case5");
 })
-test("test case1",async({page})=>
+test("test case6",async({page})=>
 {
-    console.log("This is my test case1");
+    console.log("This is my test case6");
 })
