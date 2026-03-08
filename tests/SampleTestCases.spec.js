000a55149777cc1d53a91463ed5cda32d1d7e913
divyavcubetesting <divyavcubetesting@gmail.com>
2026-03-08 21:44:20 +0530

    pushing my code

---
+++ tests/SampleTestCases.spec.js
@@ -0,0 +1,26 @@
+import { test, expect } from '@playwright/test'
+
+test("test case1",async({page})=>
+{
+    console.log("This is my test case1");
+})
+test("test case1",async({page})=>
+{
+    console.log("This is my test case1");
+})
+test("test case1",async({page})=>
+{
+    console.log("This is my test case1");
+})
+test("test case1",async({page})=>
+{
+    console.log("This is my test case1");
+})
+test("test case1",async({page})=>
+{
+    console.log("This is my test case1");
+})
+test("test case1",async({page})=>
+{
+    console.log("This is my test case1");
+})
